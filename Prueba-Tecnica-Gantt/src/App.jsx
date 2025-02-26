79df5e3a83aecb7d10bcc1080643f082ccd5b334
miguelba18 <miguel_bahamonro@fet.edu.co>
2025-02-25 22:29:20 -0500

    prueba final de entorno y github

--- Prueba-Tecnica-Gantt/src/App.jsx
+++ Prueba-Tecnica-Gantt/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
   
       <div>
-        <h1 className='text-green-500 text-4xl'>Prueba tailwind</h1>
+        <h1 className='text-green-500 text-4xl'>Prueba tailwind y prueba de github</h1>
       </div>
       
   )
